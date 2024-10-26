e751247dbf26ac1c01cd10c8304a4b2c5d05bdcf
hje <hjerikson@gmail.com>
2024-10-26 10:42:50 +0200

    Added renderGameBoard function to render the basic board

---
+++ src/dom.js
@@ -0,0 +1,26 @@
+// Clears and rerenders the board
+export function renderGameBoard() {
+  // Clear content first 
+  let content = document.getElementById('content');
+  content.innerHTML = '';
+
+  // Container div
+  const container = document.createElement('div');
+  container.classList.add('board-container');
+  
+  // 10x10 grid
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+
+      const boardCell = document.createElement('div');
+      boardCell.classList.add('board-cell');
+      boardCell.id = `cell-${x}-${y}`;
+      container.append(boardCell);
+      
+    }
+  }
+
+  // Attach to content
+  content = document.getElementById('content');
+  content.append(container);
+}
